f8d20aab93c668e09992cee465fa5c52a8853479
Jordan Rousseau <jrousseau@wdtinc.com>
2014-11-20 13:47:41 -0600

    fixing some canvas resizeing issues. Reset heatmap on canvas resize

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ var webgl_heatmap = require('./bin/webgl-heatmap');
 var req_anim_frame = window.requestAnimationFrame || window.mozRequestAnimationFrame;
 
 function HM(map) {
-    var heatmap_canvas = document.createElement('canvas');
-    heatmap_canvas.classList.add('heatmap-canvas');
-    heatmap_canvas.style.position = 'absolute';
-    heatmap_canvas.style.top = '0px';
-    heatmap_canvas.style.bottom = '0px';
-    heatmap_canvas.style.width = '100%';
-    heatmap_canvas.style.height = '100%';
-    map.container.appendChild(heatmap_canvas);
-
+    this.heatmap_canvas = document.createElement('canvas');
+    this.heatmap_canvas.classList.add('heatmap-canvas');
+    map.container.appendChild(this.heatmap_canvas);
+    this.heatmap_canvas.setAttribute('style', 'position: absolute;');
     map.on('move', (this._resetHeatmap).bind(this));
     map.on('movestart', (this._resetHeatmap).bind(this));
     map.on('moveend', (this._resetHeatmap).bind(this));
@@ -19,9 +14,10 @@ function HM(map) {
 
 
     this.heatmap = webgl_heatmap.createWebGLHeatmap({
-        canvas: heatmap_canvas
+        canvas: this.heatmap_canvas
     });
 
+    this._resizeCanvas(map.container.offsetWidth, map.container.offsetHeight);
     req_anim_frame((this._updateHeatmap).bind(this));
 };
 
@@ -35,13 +31,32 @@ HM.prototype._updateHeatmap = function () {
 }
 
 HM.prototype._resetHeatmap = function (event) {
+    if (event.type === 'resize') {
+        this._resizeCanvas(event.target.container.offsetWidth, event.target.container.offsetHeight);
+    }
     this.heatmap.multiply(0.5);
     this.heatmap.update();
     this.heatmap.display();
     this.heatmap.clamp(0.0, 1.0);
     this.heatmap.blur();
 }
+
+HM.prototype._resizeCanvas = function (width, height) {
+    var pixelRatio = window.devicePixelRatio || 1;
+
+    // Request the required canvas size taking the pixelratio into account.
+    this.heatmap_canvas.width = pixelRatio * width;
+    this.heatmap_canvas.height = pixelRatio * height;
+
+    // Maintain the same canvas size, potentially downscaling it for HiDPI displays
+    this.heatmap_canvas.style.width = width + 'px';
+    this.heatmap_canvas.style.height = height + 'px';
+
+    this.heatmap.adjustSize();
+}
+
 HM.prototype.addPoint = function (x, y, size, intensity) {
+    console.log(x + ' - ' + y);
     this.heatmap.addPoint(x, y, size, intensity);
 }
 
